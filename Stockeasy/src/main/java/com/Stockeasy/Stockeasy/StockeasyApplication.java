41dbb9e446bc1a548f9c5419a806997e183a0c73
Pedro Josué <pedro.josue.ap@outlook.com>
2025-10-06 21:57:16 -0300

    Commit inicial

---
+++ Stockeasy/src/main/java/com/Stockeasy/Stockeasy/StockeasyApplication.java
@@ -0,0 +1,13 @@
+package com.Stockeasy.Stockeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockeasyApplication.class, args);
+	}
+
+}
---
+++ Stockeasy/src/test/java/com/Stockeasy/Stockeasy/StockeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Stockeasy.Stockeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
