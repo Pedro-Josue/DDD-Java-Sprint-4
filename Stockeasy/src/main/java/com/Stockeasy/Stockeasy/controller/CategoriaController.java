e5ee3510727f2f94fb3331bbd269d97c02bad4d2
Pedro Josué <pedro.josue.ap@outlook.com>
2025-10-07 12:09:16 -0300

    Controller e Service de categoria feita e testada.

--- Stockeasy/src/main/java/com/Stockeasy/Stockeasy/controller/CategoriaController.java
+++ Stockeasy/src/main/java/com/Stockeasy/Stockeasy/controller/CategoriaController.java
@@ -1,4 +1,40 @@
 package com.Stockeasy.Stockeasy.controller;
 
+import com.Stockeasy.Stockeasy.model.Categoria;
+import com.Stockeasy.Stockeasy.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
+    @Autowired
+    private CategoriaService categoriaService;
+
+    //listar todas as categorias
+    @GetMapping
+    public List<Categoria> listaCategorias(){
+        return categoriaService.listaCategoria();
+    }
+
+    //criar categoria
+    @PostMapping("/novo")
+    public ResponseEntity<String> criaCategoria(@RequestBody Categoria categoria){
+        return categoriaService.criaCategoria(categoria);
+    }
+
+    //editar categoria existente
+    @PutMapping("/edita/{id}")
+    public ResponseEntity<String> editaCategoria(@PathVariable Long id, @RequestBody Categoria categoria){
+        return categoriaService.editaCategoria(id, categoria);
+    }
+
+    //exclui categoria
+    @DeleteMapping("/exclui/{id}")
+    public ResponseEntity<String> excluiCategoria(@PathVariable Long id){
+        return categoriaService.excluiCategoria(id);
+    }
 }
--- Stockeasy/src/main/java/com/Stockeasy/Stockeasy/service/CategoriaService.java
+++ Stockeasy/src/main/java/com/Stockeasy/Stockeasy/service/CategoriaService.java
@@ -1,4 +1,52 @@
 package com.Stockeasy.Stockeasy.service;
 
+import com.Stockeasy.Stockeasy.model.Categoria;
+import com.Stockeasy.Stockeasy.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CategoriaService {
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public List<Categoria> listaCategoria() {
+        return categoriaRepository.findAll();
+    }
+
+    public ResponseEntity<String> criaCategoria(Categoria categoria) {
+        categoriaRepository.save(categoria);
+        return ResponseEntity.status(201).body("Categoria criada com sucesso");
+    }
+
+    public ResponseEntity<String> editaCategoria(Long id, Categoria categoria) {
+        Optional<Categoria> categoriaAntiga = categoriaRepository.findById(id);
+        if (categoriaAntiga.isPresent()){
+            Categoria existente = categoriaAntiga.get();
+            existente.setNome(categoria.getNome());
+            categoriaRepository.save(existente);
+            return ResponseEntity.status(200).body("Alterações salvas com sucesso");
+        } else {
+            return ResponseEntity.status(404).body("Categoria não encontrada");
+        }
+    }
+
+    public ResponseEntity<String> excluiCategoria(Long id) {
+        Optional<Categoria> categoria = categoriaRepository.findById(id);
+        if (categoria.isPresent()){
+            Categoria existente = categoria.get();
+            if (existente.getMateriais() != null && !existente.getMateriais().isEmpty()) {
+                return ResponseEntity.status(400).body("Não é possível excluir uma categoria que possui materiais associados.");
+            }
+            categoriaRepository.delete(existente);
+            return ResponseEntity.status(200).body("Categoria excluída com sucesso");
+        } else {
+            return ResponseEntity.status(404).body("Categoria não encontrada");
+        }
+    }
 }
